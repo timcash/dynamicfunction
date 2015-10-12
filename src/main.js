a9753c2e0c3f58b08925c62cd9980a1ee883a86d
Tim Cash <timcash@gmail.com>
2015-10-12 11:44:59 -0700

    finish basic setup

--- src/main.js
+++ src/main.js
@@ -1,4 +1,23 @@
-import {bootstrap} from './bootstrap.js';
+import {bootstrap}  from './bootstrap.js';
+import _Fetch       from "isomorphic-fetch";
+import _Promise     from "bluebird";
+
 bootstrap();
 
-console.log("foobar")
+console.log("foobar");
+
+let funcs = ["a", "b", "return a + b"]
+
+let f = new Function(...funcs)
+
+let ans = f(3, 4)
+
+console.log(ans);
+
+async function foo() {
+    let a = await fetch("https://api.npmjs.org/downloads/point/last-day");
+    let b = await a.json();
+    console.log(b);
+}
+
+foo();
